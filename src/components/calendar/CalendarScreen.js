201462980947f55b4f8d9e06373e8bca6ba8c0e8
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-05-09 19:32:54 -0300

    config on calendar

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -2,7 +2,11 @@ import React from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import Navbar from "../ui/Navbar";
+import { messages } from "../../helpers/calendar-messages-es";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import "moment/locale/es";
+
+moment.locale("es");
 
 const localizer = momentLocalizer(moment); // or globalizeLocalizer
 const events = [
@@ -15,6 +19,18 @@ const events = [
 ];
 
 const CalendarScreen = () => {
+  const eventStyleGeter = (event, start, end, idSelected) => {
+    const style = {
+      backgroundColor: "#367CF7",
+      borderRadius: "0px",
+      opacity: 0.8,
+      display: "block",
+      color: "white",
+    };
+
+    return { style };
+  };
+
   return (
     <div className="calendar-screen">
       <Navbar />
@@ -23,6 +39,8 @@ const CalendarScreen = () => {
         events={events}
         startAccessor="start"
         endAccessor="end"
+        messages={messages}
+        eventPropGetter={eventStyleGeter}
       />
     </div>
   );
