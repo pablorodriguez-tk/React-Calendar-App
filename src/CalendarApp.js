8cece5280e23bda3c8c3021825307dfe3f6304ac
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-05-09 17:11:42 -0300

    Add initial config to the  Calendar

---
+++ src/CalendarApp.js
@@ -0,0 +1,8 @@
+import React from "react";
+import AppRouter from "./router/AppRouter";
+
+const CalendarApp = () => {
+  return <AppRouter />;
+};
+
+export default CalendarApp;
---
+++ src/components/calendar/CalendarScreen.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import Navbar from "../ui/Navbar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const localizer = momentLocalizer(moment); // or globalizeLocalizer
+const events = [
+  {
+    title: "Cumple del jefe",
+    start: moment().toDate(),
+    end: moment().add(2, "hours").toDate(),
+    bgcolor: "#fafafa",
+  },
+];
+
+const CalendarScreen = () => {
+  return (
+    <div className="calendar-screen">
+      <Navbar />
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+      />
+    </div>
+  );
+};
+
+export default CalendarScreen;
